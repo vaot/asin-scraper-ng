162cfd536a488eb92f3271ee2ebffb341150c74b
vaot <victorandrey_123@hotmail.com>
2019-06-22 19:51:34 -0700

    Include apiHost for prod env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://api.victorvaot.com"
-};
+  apiUrl: "http://api.victorvaot.com",
+  apiHost: "api.victorvaot.com"
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiUrl: "http://localhost:3000",
   apiHost: "localhost:3000"
-};
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
